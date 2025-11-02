2816607c314c18d4ade06b5fe4d6ff5e114f4428
majidlive1989 <majid.live@gmail.com>
2025-11-03 00:04:51 +0330

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import { useTodo } from "./store/useTodo";
-import { log } from "console";
+
 
 function App() {
-  const { todo, setTodo } = useTodo();
-  const { titleTodo, setTitleTodo } = useState("");
-  console.log(titleTodo);
+ const { todo, setTodo } = useTodo();
+ const { titleTodo, setTitleTodo } = useState("");
+ 
   return (
     <div className="flex flex-col justify-center   ">
       <div className="flex flex-col justify-center bg-white gap-3 p-8  shadow-md w-[560px]">
         <h1 className="text-[#063251] font-bold self-center">Grocery Bud</h1>
 
-        <form onSubmit={() => {}} className="flex self-stretch" action="">
-          <input
-            value={titleTodo}
-            onChange={(e) => {
-              setTitleTodo(e.target.value);
-            }}
+        <form onSubmit={(e) => {
+              e.preventDefault();
+              setTodo({ id: uuidv4(), title: title });
+              setTitleTodo("");
+            }} className="flex self-stretch" action="">
+          <input value={titleTodo}
+          onChange={(m)=>{
+            setTitleTodo(m.target.value)
+            console.log(setTitleTodo)
+          }}
+           
             className="border-2 border-[#617d98]  rounded-l-md p-0.5 "
             type="text"
             placeholder="e.g. bread"
@@ -26,6 +31,11 @@ function App() {
             Submit
           </button>
         </form>
+         <ul className="flex flex-col">
+            {todo.map((item) => (
+              <li>{item.title}</li>
+            ))}
+          </ul>
       </div>
     </div>
   );
